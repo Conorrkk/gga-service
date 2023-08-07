bf21ce263284699308bb9c7adfad53e6ff50b894
Conorrkk <123377115+Conorrkk@users.noreply.github.com>
2023-08-07 10:18:16 +0100

    match creation functionality added and get players functionality added

--- routes/matches.js
+++ routes/matches.js
@@ -1,78 +1,90 @@
-const express = require('express')
-const router = express.Router()
-const Match = require('../models/match')
+const express = require("express");
+const router = express.Router();
+const Match = require("../models/match");
+const auth = require("../middleware/authentication");
 
 // getting all
-router.get('/', async (req, res) => {
-    try {
-        const matches = await Match.find()
-        res.json(matches)
-    } catch (err){
-        res.status(500).json({message: err.message})
-    }
-})
+router.get("/", async (req, res) => {
+  try {
+    const matches = await Match.find();
+    res.json(matches);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 // getting one
-router.get('/:id', getMatch, (req, res) => {
-    res.send(res.match.team_1.team_name)
-})
+router.get("/:id", auth.reqAuth, async (req, res) => {
+  try {
+    const match = await Match.findById(req.params.id);
+    if (match == null) {
+      return res.status(404).json({ message: "Cannot find match" });
+    }
+    res.json(match);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 // creating one
-router.post('/', async (req, res) => {
-    console.log('adding matches')
-    console.log(req.body)
-    const match = new Match({
-        teams: {
-            oppositionTeam: req.body.opponent,
-            teamId: req.body.teamId,
-            userId: req.body.userId
-        }
-    })
-    try {
-        const newMatch = await match.save()
-        res.status(201).json(newMatch)
-    } catch (err) {
-        res.status(400).json({ message: err.message })
-    }
-})
+router.post("/", auth.reqAuth, async (req, res) => {
+  console.log("adding match");
+  console.log(req.body);
+  const match = new Match({
+    teams: {
+      oppositionTeam: req.body.opponent,
+      teamId: req.body.team._id,
+    },
+    userId: req.userId,
+  });
+  try {
+    const newMatch = await match.save();
+    res.status(201).json(newMatch);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
-// updating one
-router.patch('/:id', getMatch, async (req, res) => {
-    if (req.body.teamOneName != null) {
-        res.match.team_1.team_name = req.body.teamOneName
-    }
-    if (req.body.teamTwoName != null) {
-        res.match.team_2.team_name = req.body.teamTwoName
-    }
-    try {
-        const updatedMatch = await res.match.save()
-        res.json(updatedMatch)
-    } catch (err){
-        res.status(400).json({ message: err.message})
-    }
+// adding players to a match based on id
+router.patch("/:id/addPlayers", auth.reqAuth, async (req, res) => {
+  try {
+    const matchId = req.params.id;
+    const playerId = req.body.playerId;
+    console.log("player to add:", playerId);
+    const updatedMatch = await Match.findOneAndUpdate(
+      // matching the match id with the one passed in
+      { _id: matchId },
+      { $push: { "teams.players": { playerId } } }, // Add the players to the specific team
+      { new: true } // Return the updated match
+    );
 
-})
+    res.json(updatedMatch);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 // deleting one
-router.delete('/:id', getMatch, async (req, res) => {
-    try {
-        await res.match.deleteOne()
-        res.json({ message: 'Deleted Match'})
-    } catch (err) {
-        res.status(500).json({ message: err.message })
-    }
-})
+router.delete("/:id", getMatch, async (req, res) => {
+  try {
+    await res.match.deleteOne();
+    res.json({ message: "Deleted Match" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
-async function getMatch (req, res, next) {
-    let match 
-    try {
-        match = await Match.findById(req.params.id)
-        if (match==null) {
-            return res.status(404).json({ message: 'Cannot find match'})
-        }
-    } catch (err) {
-        return res.status(500).json({ message: err.message})
+async function getMatch(req, res, next) {
+  let match;
+  try {
+    match = await Match.findById(req.params.id);
+    if (match == null) {
+      return res.status(404).json({ message: "Cannot find match" });
     }
-    res.match = match
-    next()
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+  res.match = match;
+  next();
 }
 
-module.exports = router
+module.exports = router;
--- routes/players.js
+++ routes/players.js
@@ -1,30 +1,43 @@
-const express = require('express')
-const router = express.Router()
-const Player = require('../models/player')
+const express = require("express");
+const router = express.Router();
+const Player = require("../models/player");
 const auth = require("../middleware/authentication");
 
 router.post("/", auth.reqAuth, async (req, res) => {
-    const player = new Player({
-      playerName: req.body.playerName,
-      playerPosition: req.body.playerPosition,
-      teamId: req.body.teamId,
-    });
-  
-    console.log("creating player with:", player);
-  
-    try {
-      const newPlayer = await player.save();
-      res.status(201).json(newPlayer);
-    } catch (err) {
-      // mongodb error 11000 is a dublicate key error - might not need this here - what if there is a team with 2 players with same name and position?
+  const player = new Player({
+    playerName: req.body.playerName,
+    playerPosition: req.body.playerPosition,
+    teamId: req.body.teamId,
+  });
+
+  console.log("creating player with:", player);
+
+  try {
+    const newPlayer = await player.save();
+    res.status(201).json(newPlayer);
+  } catch (err) {
+    // mongodb error 11000 is a dublicate key error - might not need this here - what if there is a team with 2 players with same name and position?
     //   if (err.code === 11000) {
     //     return res
     //       .status(400)
     //       .json({ message: "player already created" });
     //   }
-      res.status(400).json({ message: err.message });
-    }
-  });
+    res.status(400).json({ message: err.message });
+  }
+});
 
+router.get("/", auth.reqAuth, async (req, res) => {
+  console.log("trying to get players")
+ 
+  const { teamId } = req.query;
+  console.log(teamId)
+  try {
+    const players = await Player.find({ teamId : teamId });
+    console.log(players)
+    res.json(players);
+  } catch (err) {
+    res.status(500).json({ message: "error interacting with db" });
+  }
+});
 
-module.exports = router
+module.exports = router;
